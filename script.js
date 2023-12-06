2e285fc77e3265eaa35240f7cff085a96dee7061
Ebere330 <eberechukuadekogbe@gmail.com>
2023-12-06 23:41:51 +0100

    Assignment-classwork

---
+++ script.js
@@ -0,0 +1,45 @@
+function calConverter() {
+    const amount = prompt('Enter Amount to be converted');
+    const currency = prompt('Enter Currency');
+    const exchangeRate = prompt('Enter Exchange Rate');
+     money = exchangeRate * amount;
+     console.log(money + ' ' + currency);
+}
+
+calConverter(); 
+
+
+// function currencyConverter() {
+   
+//    let currency = prompt("Enter NGN if naira or USD if dollar");
+//    let amount = prompt(("Enter Amount to be converted"));
+//    let exchangeRate = prompt(("Enter Exchange Rate to be converted"));
+    
+//     if (currency = "NGN" || "USD"); {
+        
+//         // const exchangeRate = 100
+
+//         // let calExchangedRate;
+
+//         let converter = amount * exchangeRate;
+//         // let userRate = amount * exchangeRate;
+//         // console.log(userRate);
+       
+        
+//     } return converter;
+    
+
+    
+        
+
+       
+        
+    
+    
+// }
+// console.log(currencyConverter());
+
+
+
+
+
